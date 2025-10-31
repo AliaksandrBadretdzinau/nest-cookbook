4c1842e576c8402e8ebd0709b524fc92b10badbb
Aliaksandr <aliaksandr.badretdzinau@on-running.com>
2025-10-31 14:29:14 +0100

    Add solution to Modules section

--- 01-modules/src/app.module.ts
+++ 01-modules/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { GuildsModule } from './guilds/guilds.module';
+import { QuestsModule } from './quests/quests.module';
 
 @Module({
-  imports: [],
+  imports: [GuildsModule, QuestsModule],
   controllers: [],
   providers: [],
 })
